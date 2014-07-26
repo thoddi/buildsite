2cb8e6c6f250071f221dbc4b569cc0000b46c9b0
thoddi <thorvardur13@ru.is>
2014-07-26 22:58:48 +0000

    Build tafla

---
+++ BuildSite/BuildSite/Models/Build.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildSite.Models
+{
+    public class Build
+    {
+        int id;
+        string name;
+        string heroClass;
+        Item[] item = new Item[13];
+        int element;
+        DateTime date;
+        string version;
+        int upvotes;
+        bool hardCore;
+        string contributor;
+        string link;
+        string[] paragonLevel = new string[12];
+    }
+}
