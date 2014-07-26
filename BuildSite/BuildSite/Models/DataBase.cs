fa8a65a9ec1eed8df9c27344bfbb439408c9edcf
thoddi <thorvardur13@ru.is>
2014-07-26 22:59:17 +0000

    database tafla

---
+++ BuildSite/BuildSite/Models/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BuildSite.Models
+{
+    public class DataBase : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Build> Builds { get; set; }
+        public DbSet<Officiality> Officialities { get; set; }
+    }
+}
