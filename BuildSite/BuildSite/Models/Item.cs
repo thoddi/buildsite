3cc129beb1c193009782fe37530ac00b4ab7fd1b
thoddi <thorvardur13@ru.is>
2014-07-26 22:59:47 +0000

    item tafla

---
+++ BuildSite/BuildSite/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildSite.Models
+{
+    public class Item
+    {
+        int id;
+        string name;
+        int slot;
+        string[] primaryStat = new string[5];
+        string[] secondaryStat = new string[3];
+        int priority;
+    }
+}
---
+++ BuildSite/BuildSite/Models/Officiality.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildSite.Models
+{
+    public class Officiality
+    {
+        int id;
+        bool officiality;
+    }
+}
