557226ba075b740711f41b2c65f96cdc5537dd3a
SveinbjornB <sveimbi@gmail.com>
2014-07-28 22:35:42 +0000

    ruff frontpage

--- BuildSite/BuildSite/App_Start/BundleConfig.cs
+++ BuildSite/BuildSite/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace BuildSite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/NavigationBar.css"));
         }
     }
 }
