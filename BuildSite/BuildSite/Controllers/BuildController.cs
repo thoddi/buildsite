7ec5648f9a55c1e740b86a2a37ccf38c55a34e41
SveinbjornB <sveimbi@gmail.com>
2014-07-30 00:56:17 +0000

    Needs work

---
+++ BuildSite/BuildSite/Controllers/BuildController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildSite.Controllers
+{
+    public class BuildController : Controller
+    {
+        public ActionResult Builder()
+        {
+            return View();
+        }
+
+        public ActionResult Build()
+        {
+            return View();
+        }
+    }
+}
---
+++ BuildSite/BuildSite/Controllers/HeroController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildSite.Controllers
+{
+    public class HeroController : Controller
+    {
+        public ActionResult HeroPage()
+        {
+            return View();
+        }
+    }
+}
